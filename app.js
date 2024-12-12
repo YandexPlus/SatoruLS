61d68f98b9963609f4b0b9432874f70dd7a81551
YandexPlus <yandexplus360@gmail.com>
2024-12-12 22:16:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.get('/', async (req, res) => {
 });
 
 // Запуск сервера
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Сервер запущен на порту ${PORT}`);
-}); 
+});
