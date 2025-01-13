a35529c9516b503434b7d986494b90e177be622d
YandexPlus <yandexplus360@gmail.com>
2025-01-14 02:27:50 +0300

    Update initDB.js

--- config/initDB.js
+++ config/initDB.js
@@ -1,13 +1,32 @@
+// const knex = require('knex');
+// const db = knex({
+//     client: 'postgresql',
+//     connection: {
+//         host: '176.108.252.232',
+//         port: 5432,
+//         database: 'satorubase',
+//         user: 'postgres',
+//         password: 'satoruls',
+//         ssl: false
+//     },
+//     pool: {
+//         min: 2,
+//         max: 10
+//     }
+// });
+
+// module.exports = { db }; 
+
+
 const knex = require('knex');
+
 const db = knex({
     client: 'postgresql',
     connection: {
-        host: '176.108.252.232',
-        port: 5432,
-        database: 'satorubase',
-        user: 'postgres',
-        password: 'satoruls',
-        ssl: false
+        connectionString: 'postgresql://postgres:wUCVMOsoNwlDmkdMIgPUMiQVLFlGGzxX@monorail.proxy.rlwy.net:44202/railway',
+        ssl: {
+            rejectUnauthorized: false // Если подключение через SSL
+        }
     },
     pool: {
         min: 2,
@@ -15,4 +34,4 @@ const db = knex({
     }
 });
 
-module.exports = { db }; 
+module.exports = { db };
